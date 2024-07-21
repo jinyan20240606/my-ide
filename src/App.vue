a02117a436297bc32f92b9da70413e2fed3577c3
jinyan1 <jinyan1@360.cn>
2024-07-21 18:30:09 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import monacoEditor from "./monacoEditor.vue";
-const value = ref("const a = 1");
-const language = ref("javascript");
-const hightChange = ref<any>(false);
-const editorMounted = (editor: any) => {
-  console.log("editor实例加载完成", editor);
-};
 
-const onChange = (value: any) => {
-  console.log("onchange--value", value);
-};
 </script>
 
 <template>
-  <h1>-----editor---</h1>
+  <h1>---meloader--editor---</h1>
   <div class="editor-wrap">
-    <monacoEditor
-      v-model="value"
-      :language="language"
-      :hight-change="hightChange"
-      :read-only="tablist?.length === 0"
-      width="100%"
-      height="100%"
-      @editor-mounted="editorMounted"
-      @change="onChange"
-      theme="vs-dark"
-    />
+
   </div>
 </template>
 
@@ -34,20 +13,5 @@ const onChange = (value: any) => {
 .editor-wrap {
   width: 1000px;
   min-height: 500px;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
