9ed889484c12a105a5d9408fbed61867115d6a30
jinyan1 <jinyan1@360.cn>
2024-07-21 17:31:26 +0800

    monaco-editor的基础使用

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import monacoEditor from "./monacoEditor.vue";
+const value = ref("const a = 1");
+const language = ref("javascript");
+const hightChange = ref<any>(false);
+const editorMounted = (editor: any) => {
+  console.log("editor实例加载完成", editor);
+};
+
+const onChange = (value: any) => {
+  console.log("onchange--value", value);
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1>-----editor---</h1>
+  <div class="editor-wrap">
+    <monacoEditor
+      v-model="value"
+      :language="language"
+      :hight-change="hightChange"
+      :read-only="tablist?.length === 0"
+      width="100%"
+      height="100%"
+      @editor-mounted="editorMounted"
+      @change="onChange"
+      theme="vs-dark"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.editor-wrap {
+  width: 1000px;
+  min-height: 500px;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
